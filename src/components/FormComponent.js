212d8c1e0caad92fe8b544fd68150f0bf12c3881
Jon Borgonia <jon@gomagames.com>
2017-01-07 12:49:24 -1000

    Password checker form submits

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -1,19 +1,47 @@
 import Inferno from 'inferno';
+import Component from 'inferno-component';
 
-export function FormComponent() {
-	return (
-		<fieldset>
-      <div className="field">
-        <label htmlFor="username">Enter Your Username</label>
-        <input id="username" type="text" name="username" />
-      </div>
-      <div className="field">
-        <label htmlFor="password">Enter Your Password</label>
-        <input id="password" type="text" name="password" />
-      </div>
-      <div className="field">
-        <button type="button">Check</button>
-      </div>
-		</fieldset>
-	);
+export class FormComponent extends Component{
+
+ constructor() {
+    super();
+    this.state = {
+      username: null,
+      password: null,
+    };
+    this.updateUsername = this.updateUsername.bind(this);
+    this.updatePassword = this.updatePassword.bind(this);
+    this.submit = this.submit.bind(this);
+  }
+
+  updateUsername(event) {
+    this.setState({username: event.target.value});
+  }
+
+  updatePassword(event) {
+    this.setState({password: event.target.value});
+  }
+
+  submit(event) {
+    event.preventDefault();
+    // handle form submission
+  }
+
+  render() {
+    return (
+      <form action="#" onSubmit={this.submit}>
+        <div className="field">
+          <label htmlFor="username">Enter Your Username</label>
+          <input id="username" type="text" name="username" onChange={this.updateUsername} />
+        </div>
+        <div className="field">
+          <label htmlFor="password">Enter Your Password</label>
+          <input id="password" type="password" name="password" onChange={this.updatePassword} />
+        </div>
+        <div className="field">
+          <button type="submit">Check</button>
+        </div>
+      </form>
+    );
+  }
 }
