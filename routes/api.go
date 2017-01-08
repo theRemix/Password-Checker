a26a354803714194ff90177f2f0ac1d2e7fc0ee9
maliaoreta <maliaoreta@gmail.com>
2017-01-07 16:30:46 -1000

    Save user info to users.db on successful submit

--- routes/api.go
+++ routes/api.go
@@ -2,12 +2,22 @@ package routes
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"math/rand"
 	"net/http"
+
+	"github.com/boltdb/bolt"
 )
 
 type CheckResponse struct {
 	Success bool `json:"success"`
+}
+
+type UserInfo struct {
+	Username  string `json:"username"`
+	Password  string `json:"password"`
+	Timestamp string `json:"timestamp"`
 }
 
 func TestApiHandler(w http.ResponseWriter, r *http.Request) {
@@ -22,6 +32,17 @@ func TestApiHandler(w http.ResponseWriter, r *http.Request) {
 
 func CheckApiHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var userInfo *UserInfo
+	err = json.Unmarshal(body, &userInfo)
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	successChance := rand.Intn(101)
 	var checkResponse bool
@@ -38,4 +59,34 @@ func CheckApiHandler(w http.ResponseWriter, r *http.Request) {
 
 	j, _ := json.Marshal(response)
 	w.Write(j)
+	postData(userInfo)
+}
+
+func postData(userInfo *UserInfo) {
+	// Open the my.db data file in your current directory.
+	// It will be created if it doesn't exist.
+	db, openDbErr := bolt.Open("users.db", 0600, nil)
+	if openDbErr != nil {
+		fmt.Println("ERR: ", openDbErr)
+	}
+	defer db.Close()
+
+	// Store the user model in the user bucket using the username as the key.
+	updateErr := db.Update(func(tx *bolt.Tx) error {
+		b, insertErr := tx.CreateBucketIfNotExists([]byte("Users"))
+		if insertErr != nil {
+			return insertErr
+		}
+
+		encoded, encodeErr := json.Marshal(userInfo)
+		if encodeErr != nil {
+			return encodeErr
+		}
+
+		return b.Put([]byte(userInfo.Username), encoded)
+	})
+
+	if updateErr != nil {
+		fmt.Println("ERR:", updateErr)
+	}
 }
